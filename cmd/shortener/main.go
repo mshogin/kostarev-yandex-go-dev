518043374abfa41c31af92d62c8e96a39fba68fc
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-04-01 00:39:08 +0500

    simple http server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"github.com/IKostarev/yandex-go-dev/internal/handlers"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.CompressHandler)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
