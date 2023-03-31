9eedd1dc78f37fcc431309b8e7de9263f59bc644
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-04-01 00:38:49 +0500

    compression handler

---
+++ internal/handlers/compress.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"github.com/IKostarev/yandex-go-dev/internal/app"
+	"net/http"
+)
+
+func CompressHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+
+	if r.Method == http.MethodPost {
+		url := r.FormValue("url")
+
+		if len(url) != 0 {
+			m := app.SaveUrls()
+
+			for k, v := range m {
+				if url == k {
+					w.Write([]byte("http://localhost:8080/" + v))
+					w.WriteHeader(http.StatusCreated)
+				}
+			}
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if r.Method == http.MethodGet {
+		url := r.URL.String()
+
+		m := app.SaveUrls()
+
+		for k, v := range m {
+			if url == ("/" + v) {
+				w.Header().Add("Content-Location", k)
+				w.WriteHeader(http.StatusTemporaryRedirect)
+			}
+		}
+	}
+
+	w.WriteHeader(http.StatusBadRequest)
+	return
+}
