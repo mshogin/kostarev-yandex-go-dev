1350896a97d4bbd919b41ce7b5fb028cfdf254fd
IKostarev <ilya.kostarev.9@gmail.com>
2023-04-16 14:02:58 +0500

    iter5: refactor - с клиентом пока не работали, убрал не рабочий код для будущих изменений

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -14,7 +14,7 @@ func main() {
 	endpoint := "http://localhost:8080/"
 
 	// контейнер данных для запроса
-	data := url.Values{}
+	_ = url.Values{}
 
 	// приглашение в консоли
 	fmt.Println("Введите длинный URL")
@@ -27,17 +27,12 @@ func main() {
 		panic(err)
 	}
 
-	long = strings.TrimSuffix(long, "\n")
-
-	// заполняем контейнер данными
-	data.Set("url", long)
-
 	// добавляем HTTP-клиент
 	client := &http.Client{}
 	// пишем запрос
 	// запрос методом POST должен, помимо заголовков, содержать тело
 	// тело должно быть источником потокового чтения io.Reader
-	request, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(data.Encode()))
+	request, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(long))
 	if err != nil {
 		panic(err)
 	}
