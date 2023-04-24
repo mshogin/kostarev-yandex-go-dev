cb8cfbe39ed0e8f30fb289a593057c2f1662a0de
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-04-24 21:46:40 +0500

    iter7: refactor name and status code

--- internal/handlers/json.go
+++ internal/handlers/json.go
@@ -8,7 +8,7 @@ import (
 	url1 "net/url"
 )
 
-type Url struct {
+type URL struct {
 	ServerURL string `json:"url"`
 }
 
@@ -19,7 +19,7 @@ type Result struct {
 func (a *App) JSONHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	var url Url
+	var url URL
 	var res Result
 
 	if err := json.NewDecoder(r.Body).Decode(&url); err != nil {
@@ -42,7 +42,7 @@ func (a *App) JSONHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusCreated)
 	if _, err := w.Write(resp); err != nil {
 		log.Fatal("Failed to send URL on json handler")
 	}
