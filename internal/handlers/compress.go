8210bc597b47138abb4ae0505e3f2e29926d1198
IKostarev <ilya.kostarev.9@gmail.com>
2023-04-27 16:17:06 +0500

    iter8: refactor откатил изменения потому что видимо не правильно понял задание

--- internal/handlers/compress.go
+++ internal/handlers/compress.go
@@ -1,30 +1,15 @@
 package handlers
 
 import (
-	"compress/gzip"
 	"github.com/IKostarev/yandex-go-dev/internal/storage"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
-	"strings"
 )
 
 func (a *App) CompressHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
-
-	if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-		w.Header().Set("Content-Encoding", "gzip")
-		gz := gzip.NewWriter(w)
-		defer func() {
-			if err := gz.Close(); err != nil {
-				log.Println("Error closing gzip writer:", err)
-				return
-			}
-		}()
-
-		w = &GzipResponseWriter{Writer: gz, ResponseWriter: w}
-	}
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
