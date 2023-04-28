0e926c64a2dc1645b5d068bb1e42c23f16ece99c
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-04-28 11:21:03 +0500

    iter8: refactor поправил middleware и убрал его из GET запроса

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,43 +1,74 @@
 package middleware
 
 import (
-	"github.com/IKostarev/yandex-go-dev/internal/handlers"
+	"compress/gzip"
+	"io"
 	"log"
 	"net/http"
 	"strings"
 )
 
+type gzipResponseWriter struct {
+	http.ResponseWriter
+	io.Writer
+}
+
+func (w *gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func (w *gzipResponseWriter) WriteHeader(code int) {
+	w.ResponseWriter.Header().Del("Content-Length")
+	w.ResponseWriter.WriteHeader(code)
+}
+
 func GzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		originWriter := w
-		acceptEncoding := r.Header.Get("Accept-Encoding")
-		contentEncoding := r.Header.Get("Content-Encoding")
-		suppAccept := strings.Contains(acceptEncoding, "gzip")
-		suppContent := strings.Contains(contentEncoding, "gzip")
-
-		if suppContent && suppAccept {
-			compressWriter := handlers.NewCompressWriter(w)
-			originWriter = compressWriter
-			defer func() {
-				if err := compressWriter.Close(); err != nil {
-					log.Println("Error compress write:", err)
-					return
-				}
-			}()
-
-			compressReader, err := handlers.NewCompressReader(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest) //TODO поменять на 500
-			}
-
-			r.Body = compressReader
-			defer func() {
-				if err := compressReader.Close(); err != nil {
-					log.Println("Error compress reade:", err)
-					return
-				}
-			}()
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			h.ServeHTTP(w, r)
+			return
 		}
-		h.ServeHTTP(originWriter, r)
+
+		cntEncoding := r.Header.Get("Content-Encoding")
+		if !strings.Contains(cntEncoding, "gzip") {
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		cntType := r.Header.Get("Content-Type")
+		jsonType := strings.Contains(cntType, "application/json")
+		textType := strings.Contains(cntType, "text/plain")
+
+		if jsonType || textType {
+			gz, err := gzip.NewReader(r.Body)
+			if err != nil {
+				log.Printf("Error creating gzip reader: %v", err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			defer func() {
+				if err := gz.Close(); err != nil {
+					log.Printf("Error closing gzip reader: %v", err)
+					return
+				}
+			}()
+			r.Body = http.MaxBytesReader(w, gz, r.ContentLength)
+		}
+
+		w.Header().Set("Content-Encoding", "gzip")
+
+		if contentType := w.Header().Get("Content-Type"); contentType != "" {
+			w.Header().Set("Content-Type", contentType)
+		}
+
+		gz := gzip.NewWriter(w)
+		defer func() {
+			if err := gz.Close(); err != nil {
+				log.Printf("Error closing gzip writer: %v", err)
+			}
+		}()
+
+		gzw := &gzipResponseWriter{ResponseWriter: w, Writer: gz}
+		h.ServeHTTP(gzw, r)
 	}
 }
