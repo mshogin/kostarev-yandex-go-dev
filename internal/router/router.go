baa0563b6f3d4445e54ee3be24ff4758325089aa
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-05-03 22:22:32 +0500

    iter9: refactor подключил не тот логгер :)

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,22 +2,20 @@ package router
 
 import (
 	"github.com/IKostarev/yandex-go-dev/internal/handlers"
+	"github.com/IKostarev/yandex-go-dev/internal/logger"
 	"github.com/IKostarev/yandex-go-dev/internal/middleware/gzip"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func NewRouter(app handlers.App) chi.Router {
 	r := chi.NewRouter()
 
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
 	r.Use(gzip.Request)
 	r.Use(gzip.Response)
 
 	r.Route("/", func(r chi.Router) {
-		r.Post("/", app.CompressHandler)
-		r.Get("/{id}", app.GetURLHandler)
+		r.Post("/", logger.RequestLogger(app.CompressHandler))
+		r.Get("/{id}", logger.ResponseLogger(app.GetURLHandler))
 
 		r.Route("/api", func(r chi.Router) {
 			r.Post("/shorten", app.JSONHandler)
