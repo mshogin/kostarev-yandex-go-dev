7b820b4ffe7f938278f67674575fc5bb6074acf2
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-05-04 11:49:52 +0500

    iter9: refactor исправил нейминг

--- internal/handlers/json.go
+++ internal/handlers/json.go
@@ -4,42 +4,42 @@ import (
 	"encoding/json"
 	"github.com/IKostarev/yandex-go-dev/internal/logger"
 	"net/http"
-	url1 "net/url"
+	"net/url"
 )
 
-type URL struct {
+type URLRequest struct {
 	ServerURL string `json:"url"`
 }
 
-type Result struct {
+type ResultResponse struct {
 	BaseShortURL string `json:"result"`
 }
 
 func (a *App) JSONHandler(w http.ResponseWriter, r *http.Request) {
-	var url URL
-	var res Result
+	var req URLRequest
+	var resp ResultResponse
 
-	if err := json.NewDecoder(r.Body).Decode(&url); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		logger.Error("json decode is error: ", err)
 		return
 	}
 
-	short, err := a.Storage.Save(url.ServerURL)
+	short, err := a.Storage.Save(req.ServerURL)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		logger.Error("storage save is error: ", err)
 		return
 	}
 
-	res.BaseShortURL, err = url1.JoinPath(a.Config.BaseShortURL, short)
+	resp.BaseShortURL, err = url.JoinPath(a.Config.BaseShortURL, short)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		logger.Error("join path have err: ", err)
 		return
 	}
 
-	resp, err := json.Marshal(map[string]string{"result": res.BaseShortURL})
+	respContent, err := json.Marshal(resp)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		logger.Error("json marshal is error: ", err)
@@ -48,7 +48,7 @@ func (a *App) JSONHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	if _, err := w.Write(resp); err != nil {
+	if _, err := w.Write(respContent); err != nil {
 		logger.Error("Failed to send URL on json handler", err)
 	}
 }
