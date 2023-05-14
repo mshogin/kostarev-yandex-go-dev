55131dba73de1e638e551c2d0e4640262ee50833
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-05-14 21:28:16 +0500

    iter11: refactor вернул проверку существования бд

--- internal/storage/database/postgres/postgres.go
+++ internal/storage/database/postgres/postgres.go
@@ -30,16 +30,16 @@ func NewDB(addrConn string) (*DB, error) {
 		count: 1,
 	}
 
-	//exists, err := db.checkIsTablesExists()
-	//if err != nil {
-	//	return nil, fmt.Errorf("error check is table exists: %w", err)
-	//}
-	//
-	//if !exists {
-	//	if err = db.createTable(); err != nil {
-	//		return nil, fmt.Errorf("error create tables: %w", err)
-	//	}
-	//}
+	exists, err := db.checkIsTablesExists()
+	if err != nil {
+		return nil, fmt.Errorf("error check is table exists: %w", err)
+	}
+
+	if !exists {
+		if err = db.createTable(); err != nil {
+			return nil, fmt.Errorf("error create tables: %w", err)
+		}
+	}
 
 	return db, nil
 }
@@ -108,7 +108,7 @@ func (db *DB) checkIsTablesExists() (bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
 	defer cancel()
 
-	row := db.db.QueryRow(ctx, "SELECT EXISTS (SELECT COUNT(*) FROM yandex)")
+	row := db.db.QueryRow(ctx, "SELECT COUNT(*) FROM yandex")
 
 	var res int
 
