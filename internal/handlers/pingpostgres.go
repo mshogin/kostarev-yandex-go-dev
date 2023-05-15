eaeff6c17150cc1b3b4f5356a98442d30c2cdd2d
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-05-15 22:31:16 +0500

    iter11: refactor fix ping handler

--- internal/handlers/pingpostgres.go
+++ internal/handlers/pingpostgres.go
@@ -1,12 +1,18 @@
 package handlers
 
 import (
+	"github.com/IKostarev/yandex-go-dev/internal/logger"
 	"github.com/IKostarev/yandex-go-dev/internal/storage/database/postgres"
 	"net/http"
 )
 
 func (a *App) PingHandler(w http.ResponseWriter, _ *http.Request) {
-	db := &postgres.DB{}
+	db, err := postgres.NewPostgresDB(a.Config.FileStoragePath)
+	if err != nil {
+		logger.Errorf("error ping handler: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
 	if db.Pool() {
 		w.WriteHeader(http.StatusOK)
 	} else {
